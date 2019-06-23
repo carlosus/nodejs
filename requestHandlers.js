f4bf8ce082fe0c32fb01d0fdcc1099074e4ceaa9
Carlos Us Santiago <cdus12@gmail.com>
2019-06-23 15:23:51 -0700

    handled post data

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
-var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
 
     var body = '<html>'+
@@ -16,17 +16,16 @@ function start(response) {
         '</body>'+
         '</html>';
 
-    // exec("ls - lah", function(error, stdout, stderr) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(body);
-        response.end();
-    // });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log("Request handler 'upload' was called.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello Upload");
+    response.write("You've sent: "+
+    querystring.parse(postData).text);
     response.end();
 }
 
